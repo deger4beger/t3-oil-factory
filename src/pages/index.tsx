8311d176c9e1c012fd5d0f00fd471e2080a06f8a
deger4beger <deger.begerrr@gmail.com>
2022-09-16 22:31:20 +0600

    Wrapper added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -7,15 +6,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>t3-store</title>
-        <meta name="description" content="t3-store" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="">
-        main content here
-      </main>
+      Main page { hello.data?.greeting }
     </>
   );
 };
