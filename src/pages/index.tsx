8d6ed9d318415873d8677f048753a9f97c96ac8e
deger4beger <deger.begerrr@gmail.com>
2022-09-20 00:35:10 +0600

    -latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
+
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
   const test = trpc.useQuery(["example.test", { someData: "myData" }])
