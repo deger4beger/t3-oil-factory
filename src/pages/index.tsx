b24c2703198a77499ba1c2ddded70675d1eb3232
brucewayne <deger.begerrr@gmail.com>
2022-10-07 11:35:54 +0600

    :cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
 import type { NextPage } from "next";
 import PageShell from "../components/page-shell";
-import { trpc } from "../utils/trpc";
 
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-  const test = trpc.useQuery(["example.test", { someData: "myData" }])
 
   return (
-    <PageShell title="home">
-      Main page { hello.data?.greeting }
-      <br />
-      Some test data: { JSON.stringify(test.data) }
+    <PageShell title="Home">
+      Home page
     </PageShell>
   );
 };
