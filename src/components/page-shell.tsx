a63bacc153239878cfe91018eb5f5f03cabc82e2
brucewayne <deger.begerrr@gmail.com>
2022-10-07 17:56:25 +0600

    logout added

--- src/components/page-shell.tsx
+++ src/components/page-shell.tsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -30,24 +30,27 @@ const PageShell = ({
 
       <AuthHandler protected={ isProtected }>
 	      <nav className="bg-zinc-800 p-3 text-zinc-50">
-	      	<div className="m-auto w-7/12 flex">
-		      	<h1 className="text-lg cursor-pointer font-medium border-2 px-2 rounded hover:text-sky-300 hover:border-sky-300">
-			        ⊙il-gas
-			      </h1>
-			      <div className="flex">
-				      { routes.map(({ name, route }) =>
-			      		<div key={ name } className="ml-3 text-sm hover:text-zinc-300 cursor-pointer font-medium flex items-center first:ml-10">
-			      			<Link href={route}>
-			      				{ name }
-			      			</Link>
-			      		</div>
-				      ) }
-			      </div>
+	      	<div className="m-auto w-7/12 flex justify-between">
+	      	 	<div className="flex">
+	      	 		<h1 className="text-lg cursor-pointer font-medium border-2 px-2 rounded hover:text-sky-300 hover:border-sky-300">
+				        ⊙il-gas
+				      </h1>
+				      <div className="flex">
+					      { routes.map(({ name, route }) =>
+				      		<div key={ name } className="ml-3 text-sm hover:text-zinc-300 cursor-pointer font-medium flex items-center first:ml-10">
+				      			<Link href={route}>
+				      				{ name }
+				      			</Link>
+				      		</div>
+					      ) }
+				      </div>
+	      	 	</div>
+			      <AuthPanel />
 		      </div>
 	      </nav>
 
 	      <main className="min-w-max min-h-screen bg-zinc-900 text-zinc-50">
-	      	<div className="m-auto w-7/12">
+	      	<div className="m-auto w-7/12 p-3 pt-6">
 	        	{ children }
 	        </div>
 	      </main>
@@ -85,7 +88,22 @@ const AuthHandler = ({
 			{ children }
 		</UserContext.Provider>
 	)
+}
 
+const AuthPanel = () => {
+
+	const { data, status } = useSession();
+
+	if (status !== "authenticated") {
+		return <div />
+	}
+
+	return (
+		<div className="flex items-center">
+			<div className="mr-4">{ data?.user?.email }</div>
+			<button onClick={() => signOut()}>Выйти</button>
+		</div>
+	)
 }
 
 export default PageShell
