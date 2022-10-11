bcf91e9feb1e16576a89abd4e3d32f64390080f4
brucewayne <deger.begerrr@gmail.com>
2022-10-11 11:56:14 +0600

    layout extended

--- src/components/group-control.tsx
+++ src/components/group-control.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 
-const GroupControl = () => {
+const GroupControl = ({
+	children,
+	title
+}: {
+	children: React.ReactNode
+	title: string
+}) => {
 	return (
-		<div>
-			Group Control
+		<div className="flex justify-end">
+			<div className="inline-flex justify-end items-end border-b-4 border-zinc-700 pb-2">
+				<h1 className="border-zinc-600 mr-6 font-semibold pl-4">{ title }</h1>
+				<div>{ children }</div>
+			</div>
 		</div>
 	)
 }
