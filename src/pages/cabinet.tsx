51fe947f58539ff75f96f7d2a676b255be489119
brucewayne <deger.begerrr@gmail.com>
2022-10-11 11:08:31 +0600

    refactor to next/image

--- src/pages/cabinet.tsx
+++ src/pages/cabinet.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next"
 import { useSession } from "next-auth/react"
+import Image from "next/image"
 import React from "react"
 import PageShell from "../components/page-shell"
 
@@ -11,9 +12,17 @@ const Cabinet: NextPage = () => {
 	return (
 		<PageShell title="Мой кабинет" isProtected>
 			<div className="flex flex-col items-center">
-				<img src={data?.user?.image!} className="rounded-full border-4 border-zinc-700" />
+				<div className="rounded-full border-4 border-zinc-700 w-24 h-24 relative">
+					<Image
+						src={data?.user?.image || ""}
+						alt="Не удалось загрузить изображение профиля"
+						layout="fill"
+    				objectFit="cover"
+    				className="rounded-full"
+					/>
+				</div>
 				<p className="mt-4 text-zinc-300 text-xl">
-					{data?.user?.name} <span className="text-zinc-400">({data?.user?.email!})</span>
+					{data?.user?.name} <span className="text-zinc-400">({data?.user?.email})</span>
 				</p>
 			</div>
 		</PageShell>
