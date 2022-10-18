03208e1f9c62b87dc20a4791322f692d3af181d6
brucewayne <deger.begerrr@gmail.com>
2022-10-18 15:55:27 +0600

    :staged layout

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next"
 import React from "react"
 import ControlPanel from "../components/control-panel"
 import GroupControl from "../components/group-control"
+import InputDatalist from "../components/input-datalist"
 import PageShell from "../components/page-shell"
 import Common from "../features/statistics/common"
 import { trpc } from "../utils/trpc"
@@ -20,7 +21,25 @@ const Statistics: NextPage = () => {
 				totalItemsCount={ (data?.purchasesCount || 0) + (data?.salesCount || 0) }
 				isLoadingStatus={ isLoading }
 				isFetchingStatus={ isFetching }
-			/>
+			>
+				<div className="inline-flex items-center">
+					<InputDatalist
+						type="date"
+						style="dark"
+						title="Выберите период"
+						value={ "" }
+						onChange={ () => void 0 }
+					/>
+					<span> - </span>
+					<InputDatalist
+						type="date"
+						style="dark"
+						title="..."
+						value={ "" }
+						onChange={ () => void 0 }
+					/>
+				</div>
+			</ControlPanel>
 			<Common />
 		</PageShell>
 	)
