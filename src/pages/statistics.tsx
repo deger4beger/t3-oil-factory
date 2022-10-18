1d0176d61c1645439428d95058790970b6de5c80
brucewayne <deger.begerrr@gmail.com>
2022-10-18 15:34:57 +0600

    base layout

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -1,12 +1,27 @@
 import { NextPage } from "next"
 import React from "react"
+import ControlPanel from "../components/control-panel"
+import GroupControl from "../components/group-control"
 import PageShell from "../components/page-shell"
+import Common from "../features/statistics/common"
+import { trpc } from "../utils/trpc"
 
 
 const Statistics: NextPage = () => {
+
+	const { data, isLoading, isFetching } = trpc.useQuery(["statistics.getTotal"])
+
 	return (
 		<PageShell title="Статистика" isProtected>
-			Statistics
+			<GroupControl title="Статистика за выбранные даты">
+
+			</GroupControl>
+			<ControlPanel
+				totalItemsCount={ (data?.purchasesCount || 0) + (data?.salesCount || 0) }
+				isLoadingStatus={ isLoading }
+				isFetchingStatus={ isFetching }
+			/>
+			<Common />
 		</PageShell>
 	)
 }
