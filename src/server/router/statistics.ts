a8c27c430c16d2fc73205d8f0f6c6638352e24b4
brucewayne <deger.begerrr@gmail.com>
2022-10-18 15:20:59 +0600

    getTotal query logic added

--- src/server/router/statistics.ts
+++ src/server/router/statistics.ts
@@ -4,6 +4,23 @@ import { createProtectedRouter } from "./context";
 export const statisticsRouter = createProtectedRouter()
 	.query("getTotal", {
 		async resolve({ ctx }) {
-			return "hello from stats"
+			const operations = await ctx.prisma.operation.findMany()
+			const purchases = operations.filter(operation =>
+				operation.operation === "PURCHASE")
+			const sales = operations.filter(operation =>
+				operation.operation === "SALE")
+			const purchasesPrice = purchases.reduce((acc, purchase) =>
+				acc += purchase.price
+			, 0)
+			const salesPrice = sales.reduce((acc, sale) =>
+				acc += sale.price
+			, 0)
+			return {
+				purchasesCount: purchases.length,
+				purchasesPrice,
+				salesCount: sales.length,
+				salesPrice,
+				outcome: salesPrice - purchasesPrice
+			}
 		}
 	})
