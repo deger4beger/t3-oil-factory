9bfacf6fd3164212f6ba0e5d970e8adfaeb0470b
deger4beger <deger.begerrr@gmail.com>
2022-10-19 18:44:19 +0600

    styles added

--- src/features/statistics/common.tsx
+++ src/features/statistics/common.tsx
@@ -13,7 +13,21 @@ const Common = ({
 }) => {
 	return (
 		<div className="mt-2">
-			common
+			<p className="flex justify-between items-center border-2 border-b-0 border-zinc-700 p-2 mb-2 rounded">
+				<span className="text-zinc-300 font-semibold">Количество закупок</span>{ purchasesCount }
+			</p>
+			<p className="flex justify-between items-center border-2 border-t-0 border-zinc-700 p-2 mb-6 rounded">
+				<span className="text-zinc-300 font-semibold">Количество продаж</span>{ salesCount }
+			</p>
+			<p className="flex justify-between items-center border-2 border-b-0 border-zinc-700 p-2 mb-2 rounded">
+				<span className="text-zinc-300 font-semibold">Сумма закупок</span>- { purchasesPrice }
+			</p>
+			<p className="flex justify-between items-center border-2 border-t-0 border-zinc-700 p-2 mb-6 rounded">
+				<span className="text-zinc-300 font-semibold">Сумма продаж</span>+ { salesPrice }
+			</p>
+			<p className={ "flex justify-between items-center border-[3px] p-2 mb-2 rounded" + ( outcome ?? 0 > 0 ? " border-emerald-700" : " border-red-700" )}>
+				<span className="text-zinc-300 font-semibold">Общий доход за указанный период</span>{ outcome ?? 0 > 0 ? "+ " : "- " }{ outcome }
+			</p>
 		</div>
 	)
 }
