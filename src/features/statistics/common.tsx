dd6cda5e40379410ef1d04395917eb9b91dccdfe
deger4beger <deger.begerrr@gmail.com>
2022-10-19 19:14:31 +0600

    styles

--- src/features/statistics/common.tsx
+++ src/features/statistics/common.tsx
@@ -12,20 +12,20 @@ const Common = ({
 	outcome?: number
 }) => {
 	return (
-		<div className="mt-2 font-semibold text-zinc-300">
-			<p className="flex justify-between items-center border-2 border-b-0 border-zinc-700 p-2 mb-2 rounded">
+		<div className="mt-2 font-semibold text-zinc-300 bg-zinc-800 p-2 rounded border-zinc-700 border-4">
+			<p className="flex justify-between items-center border-b-2 border-zinc-700 p-2 mb-2">
 				<span>Количество закупок</span>{ purchasesCount }
 			</p>
-			<p className="flex justify-between items-center border-2 border-t-0 border-zinc-700 p-2 mb-6 rounded">
+			<p className="flex justify-between items-center border-b-2 border-zinc-700 p-2 mb-6">
 				<span>Количество продаж</span>{ salesCount }
 			</p>
-			<p className="flex justify-between items-center border-2 border-b-0 border-zinc-700 p-2 mb-2 rounded">
+			<p className="flex justify-between items-center border-b-2 border-zinc-700 p-2 mb-2">
 				<span>Сумма закупок</span>- { purchasesPrice }
 			</p>
-			<p className="flex justify-between items-center border-2 border-t-0 border-zinc-700 p-2 mb-6 rounded">
+			<p className="flex justify-between items-center border-b-2 border-zinc-700 p-2 mb-6">
 				<span>Сумма продаж</span>+ { salesPrice }
 			</p>
-			<p className={ "flex justify-between items-center border-[3px] p-2 mb-2 rounded" + ( outcome ?? 0 > 0 ? " border-emerald-700" : " border-red-700" )}>
+			<p className={ "flex justify-between items-center border-b-[3px] p-2 mb-2" + ( outcome ?? 0 > 0 ? " border-emerald-700" : " border-red-700" )}>
 				<span>Общий доход за указанный период</span>{ outcome ?? 0 > 0 ? "+ " : "- " }{ outcome }
 			</p>
 		</div>
