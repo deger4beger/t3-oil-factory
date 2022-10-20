5c10b7a61a7fcf2a4c9b4c880beb2b7d5127e829
deger4beger <deger.begerrr@gmail.com>
2022-10-20 19:24:33 +0600

    styles

--- src/features/statistics/chart.tsx
+++ src/features/statistics/chart.tsx
@@ -1,4 +1,4 @@
-import { Brush, CartesianGrid, Legend, Line,
+import { CartesianGrid, Legend, Line,
 	LineChart, ResponsiveContainer,
 Tooltip, XAxis, YAxis } from "recharts"
 
@@ -8,33 +8,32 @@ const Chart = ({ data }: { data: {
 	продажи: number
 }[] | undefined }) => {
 	return (
-		<div className="border-4 border-zinc-700 bg-zinc-300 m-4 mt-8 p-6 rounded">
+		<div className="bg-zinc-900 m-4 mt-8 p-6 rounded text-zinc-50">
 			<ResponsiveContainer width="100%" height={500}>
 	      <LineChart
 	        width={500}
 	        height={300}
 	        data={data}
 	      >
-	        <CartesianGrid strokeDasharray="0 0" stroke="#CBCBCB" />
-	        <XAxis dataKey="date" stroke="black" tickLine={{ strokeWidth: 1 }}
+	        <CartesianGrid strokeDasharray="0 0" stroke="#3A3A3A" />
+	        <XAxis dataKey="date" stroke="#C7C7C7" tickLine={{ strokeWidth: 1 }}
 	          padding={{ left: 16, right: 16 }}
 	        />
-	        <YAxis yAxisId="price" stroke="black" tickCount={10} tickSize={10}
+	        <YAxis yAxisId="price" stroke="#C7C7C7" tickCount={10} tickSize={10}
 	          tickLine={{ strokeWidth: 0.5 }}
 	        />
 	        <Tooltip />
 	        <Legend iconType="rect" />
-	        <Line yAxisId="price" type="monotone" dataKey="продажи" stroke="#047857"
+	        <Line yAxisId="price" type="monotone" dataKey="продажи" stroke="#008962"
 	          dot={{ strokeWidth: 3, r: 4 }}
 	          strokeWidth={3}
 	          strokeOpacity={0.8}
 	        />
-	        <Line yAxisId="price" type="monotone" dataKey="закупки" stroke="#b91c1c"
+	        <Line yAxisId="price" type="monotone" dataKey="закупки" stroke="#C63535"
 	          dot={{ strokeWidth: 3, r: 4 }}
 	          strokeWidth={3}
 	          strokeOpacity={0.8}
 	        />
-	        <Brush dataKey="date" height={30} />
 	      </LineChart>
 	    </ResponsiveContainer>
     </div>
