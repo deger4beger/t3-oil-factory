90a1506a1c069f03eea0a86ae07c1001c66365d4
deger4beger <deger.begerrr@gmail.com>
2022-10-20 17:51:40 +0600

    refactor

--- src/components/group-control.tsx
+++ src/components/group-control.tsx
@@ -4,14 +4,14 @@ const GroupControl = ({
 	children,
 	title
 }: {
-	children: React.ReactNode
+	children?: React.ReactNode
 	title: string
 }) => {
 	return (
 		<div className="flex justify-end">
 			<div className="inline-flex justify-end items-end border-b-4 border-zinc-700 pb-2">
 				<h1 className="border-zinc-600 mr-6 font-semibold pl-4">{ title }</h1>
-				<div>{ children }</div>
+				{ !!children && <div>{ children }</div> }
 			</div>
 		</div>
 	)
--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -1,51 +1,50 @@
-import { NextPage } from "next"
-import React from "react"
-import ControlPanel from "../components/control-panel"
-import GroupControl from "../components/group-control"
-import InputDatalist from "../components/input-datalist"
-import PageShell from "../components/page-shell"
-import Chart from "../features/statistics/chart"
-import Common from "../features/statistics/common"
-import { trpc } from "../utils/trpc"
-
+import { NextPage } from "next";
+import React from "react";
+import ControlPanel from "../components/control-panel";
+import GroupControl from "../components/group-control";
+import InputDatalist from "../components/input-datalist";
+import PageShell from "../components/page-shell";
+import Chart from "../features/statistics/chart";
+import Common from "../features/statistics/common";
+import { trpc } from "../utils/trpc";
 
 const Statistics: NextPage = () => {
 
-	const { data, isLoading, isFetching } = trpc.useQuery(["statistics.getTotal"])
-	const { operationsByDate, ...commonData } = data || {}
+	const { data, isLoading, isFetching } = trpc.useQuery([
+		"statistics.getTotal",
+	]);
+	const { operationsByDate, ...commonData } = data || {};
 
 	return (
 		<PageShell title="Статистика" isProtected>
-			<GroupControl title="Статистика за выбранные даты">
-
-			</GroupControl>
+			<GroupControl title="Статистика за выбранные даты"></GroupControl>
 			<ControlPanel
-				totalItemsCount={ (data?.purchasesCount || 0) + (data?.salesCount || 0) }
-				isLoadingStatus={ isLoading }
-				isFetchingStatus={ isFetching }
+				totalItemsCount={(data?.purchasesCount || 0) + (data?.salesCount || 0)}
+				isLoadingStatus={isLoading}
+				isFetchingStatus={isFetching}
 			>
 				<div className="inline-flex items-center">
 					<InputDatalist
 						type="date"
 						style="dark"
 						title="Выберите период"
-						value={ "" }
-						onChange={ () => void 0 }
+						value={""}
+						onChange={() => void 0}
 					/>
 					<span> - </span>
 					<InputDatalist
 						type="date"
 						style="dark"
 						title="..."
-						value={ "" }
-						onChange={ () => void 0 }
+						value={""}
+						onChange={() => void 0}
 					/>
 				</div>
 			</ControlPanel>
-			<Common { ...commonData } />
-			<Chart data={ operationsByDate } />
+			<Common {...commonData} />
+			<Chart data={operationsByDate} />
 		</PageShell>
-	)
-}
+	);
+};
 
-export default Statistics
+export default Statistics;
