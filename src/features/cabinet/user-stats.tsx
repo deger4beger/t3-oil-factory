fca383d2067346b44c006164b9ba96f46f0ebeb3
brucewayne <deger.begerrr@gmail.com>
2022-10-21 11:59:43 +0600

    refactor

--- src/features/cabinet/user-stats.tsx
+++ src/features/cabinet/user-stats.tsx
@@ -1,16 +1,22 @@
 const UserStats = ({
 	purchasesCount,
-	salesCount
+	salesCount,
 }: {
 	purchasesCount?: number;
 	salesCount?: number;
 }) => {
 	return (
 		<div className="mt-4">
-			<p className="flex justify-center text-zinc-300"><h2 className="mr-1">Количество оформленных вами закупок - </h2> { purchasesCount }</p>
-			<p className="flex justify-center text-zinc-300 mt-2"><h2 className="mr-1">Количество оформленных вами продаж - </h2> { salesCount }</p>
+			<p className="flex justify-center text-zinc-300">
+				<h2 className="mr-1">Количество оформленных вами закупок - </h2>{" "}
+				{purchasesCount}
+			</p>
+			<p className="flex justify-center text-zinc-300 mt-2">
+				<h2 className="mr-1">Количество оформленных вами продаж - </h2>{" "}
+				{salesCount}
+			</p>
 		</div>
-	)
-}
+	);
+};
 
-export default UserStats
+export default UserStats;
