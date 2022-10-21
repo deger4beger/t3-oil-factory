9750cf9b8996c2b31811a8317661d78af40dd09b
brucewayne <deger.begerrr@gmail.com>
2022-10-21 12:18:49 +0600

    reset filters button added

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import Button from "../components/button";
 import ControlPanel from "../components/control-panel";
 import GroupControl from "../components/group-control";
 import InputDatalist from "../components/input-datalist";
@@ -10,7 +11,6 @@ import Common from "../features/statistics/common";
 import { trpc } from "../utils/trpc";
 
 const Statistics: NextPage = () => {
-
 	const router = useRouter();
 	const [date, setDate] = useState({
 		from: "",
@@ -19,14 +19,25 @@ const Statistics: NextPage = () => {
 
 	const { data, isLoading, isFetching } = trpc.useQuery([
 		"statistics.getTotal",
-		( date.from && date.to ) ? {
-			from: new Date(date.from),
-			to: new Date(date.to),
-		} : {},
+		date.from && date.to
+			? {
+					from: new Date(date.from),
+					to: new Date(date.to),
+			  }
+			: {},
 	]);
 	const { operationsByDate, ...commonData } = data || {};
 
-	const onSetDate = (value: { from?: string; to?: string }) => {
+	const onSetDate = (value: { from?: string; to?: string }, reset = false) => {
+		if (reset) {
+			router.push({
+				query: {
+					from: "",
+					to: "",
+				},
+			});
+			return;
+		}
 		router.push({
 			query: {
 				from: value.from || date.from,
@@ -40,11 +51,13 @@ const Statistics: NextPage = () => {
 			...date,
 			...router.query,
 		});
-	}, [router.query])
+	}, [router.query]);
 
 	return (
 		<PageShell title="Статистика" isProtected>
-			<GroupControl title="Статистика за выбранные даты" />
+			<GroupControl title="Статистика за выбранные даты">
+				<Button text="Сбросить фильтры" onClick={() => onSetDate({}, true)} />
+			</GroupControl>
 			<ControlPanel
 				totalItemsCount={(data?.purchasesCount || 0) + (data?.salesCount || 0)}
 				isLoadingStatus={isLoading}
@@ -55,16 +68,16 @@ const Statistics: NextPage = () => {
 						type="date"
 						style="dark"
 						title="Выберите период"
-						value={ date.from }
-						onChange={ (selectedDate) => onSetDate({ from: selectedDate }) }
+						value={date.from}
+						onChange={(selectedDate) => onSetDate({ from: selectedDate })}
 					/>
 					<span> - </span>
 					<InputDatalist
 						type="date"
 						style="dark"
 						title="..."
-						value={ date.to }
-						onChange={ (selectedDate) => onSetDate({ to: selectedDate }) }
+						value={date.to}
+						onChange={(selectedDate) => onSetDate({ to: selectedDate })}
 					/>
 				</div>
 			</ControlPanel>
