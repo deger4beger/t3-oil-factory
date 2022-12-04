e547f7d4acea2738de72e214a3544490927349bf
deger4beger <deger.begerrr@gmail.com>
2022-12-04 15:17:30 +0600

    style: prettier

--- src/server/router/statistics.ts
+++ src/server/router/statistics.ts
@@ -2,78 +2,87 @@ import { z } from "zod";
 import { createProtectedRouter } from "./context";
 
 const operationKeys = {
-	"SALE": "продажи",
-	"PURCHASE": "закупки"
-}
+	SALE: "продажи",
+	PURCHASE: "закупки",
+};
 
-export const statisticsRouter = createProtectedRouter()
-	.query("getTotal", {
-		input: z.object({
-			from: z.date().optional(),
-			to: z.date().optional(),
-		}),
-		async resolve({ ctx, input }) {
-			const operations = await ctx.prisma.operation.findMany({
+export const statisticsRouter = createProtectedRouter().query("getTotal", {
+	input: z.object({
+		from: z.date().optional(),
+		to: z.date().optional(),
+	}),
+	async resolve({ ctx, input }) {
+		const operations = await ctx.prisma.operation.findMany({
+			where: {
+				createdAt: {
+					gte: input.from,
+					lte: input.to,
+				},
+			},
+		});
+		const purchases = operations.filter(
+			(operation) => operation.operation === "PURCHASE"
+		);
+		const sales = operations.filter(
+			(operation) => operation.operation === "SALE"
+		);
+		const purchasesPrice = purchases.reduce(
+			(acc, purchase) => (acc += purchase.price),
+			0
+		);
+		const salesPrice = sales.reduce((acc, sale) => (acc += sale.price), 0);
+
+		const operationsByDate = (
+			await ctx.prisma.operation.groupBy({
 				where: {
 					createdAt: {
 						gte: input.from,
-						lte: input.to
-					}
-				}
-			})
-			const purchases = operations.filter(operation =>
-				operation.operation === "PURCHASE")
-			const sales = operations.filter(operation =>
-				operation.operation === "SALE")
-			const purchasesPrice = purchases.reduce((acc, purchase) =>
-				acc += purchase.price
-			, 0)
-			const salesPrice = sales.reduce((acc, sale) =>
-				acc += sale.price
-			, 0)
-
-			const operationsByDate = (await ctx.prisma.operation.groupBy({
-				where: {
-					createdAt: {
-						gte: input.from,
-						lte: input.to
-					}
+						lte: input.to,
+					},
 				},
 				orderBy: {
-					createdAt: "asc"
+					createdAt: "asc",
 				},
 				by: ["createdAt", "operation"],
 				_sum: {
-					price: true
-				}
-			})).map(operations => ({
+					price: true,
+				},
+			})
+		)
+			.map((operations) => ({
 				date: operations.createdAt.toLocaleDateString(),
-				[operationKeys[operations.operation]]: operations._sum.price
-			})).reduce((acc, operations) => {
-				const existed = acc.find(operation =>
-					operation.date === operations.date)
-				if (!existed) acc.push({
-					"закупки": 0,
-					"продажи": 0,
-					...operations
-				})
-				if (existed) {
-					Object.assign(existed, operations)
-				}
-				return acc
-			}, [] as {
-				date: string
-				"закупки": number
-				"продажи": number
-			}[])
+				[operationKeys[operations.operation]]: operations._sum.price,
+			}))
+			.reduce(
+				(acc, operations) => {
+					const existed = acc.find(
+						(operation) => operation.date === operations.date
+					);
+					if (!existed)
+						acc.push({
+							закупки: 0,
+							продажи: 0,
+							...operations,
+						});
+					if (existed) {
+						Object.assign(existed, operations);
+					}
+					return acc;
+				},
+				[] as {
+					date: string;
+					закупки: number;
+					продажи: number;
+				}[]
+			);
 
-			return {
-				purchasesCount: purchases.length,
-				purchasesPrice,
-				salesCount: sales.length,
-				salesPrice,
-				outcome: salesPrice - purchasesPrice,
-				operationsByDate
-			}
-		}
-	})
+		return {
+			purchasesCount: purchases.length,
+			purchasesPrice,
+			salesCount: sales.length,
+			salesPrice,
+			outcome: salesPrice - purchasesPrice,
+			operationsByDate,
+		};
+	},
+});
