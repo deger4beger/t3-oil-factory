60efc895cee991b4da9cb648aebcea356175f837
deger4beger <deger.begerrr@gmail.com>
2022-12-31 15:14:28 +0600

    style: cabinet router prettier

--- src/server/router/cabinet.ts
+++ src/server/router/cabinet.ts
@@ -1,17 +1,19 @@
 import { createProtectedRouter } from "./context";
 
-
-export const cabinetRouter = createProtectedRouter()
-	.query("getInfo", {
-		async resolve({ ctx }) {
-			const operations = await ctx.prisma.operation.findMany({
-				where: {
-					userId: ctx.session.user.id,
-				}
-			})
-			return {
-				purchasesCount: operations.filter(operation =>  operation.operation === "PURCHASE").length,
-				salesCount: operations.filter(operation =>  operation.operation === "SALE").length,
-			}
-		}
-	})
+export const cabinetRouter = createProtectedRouter().query("getInfo", {
+	async resolve({ ctx }) {
+		const operations = await ctx.prisma.operation.findMany({
+			where: {
+				userId: ctx.session.user.id,
+			},
+		});
+		return {
+			purchasesCount: operations.filter(
+				(operation) => operation.operation === "PURCHASE"
+			).length,
+			salesCount: operations.filter(
+				(operation) => operation.operation === "SALE"
+			).length,
+		};
+	},
+});
